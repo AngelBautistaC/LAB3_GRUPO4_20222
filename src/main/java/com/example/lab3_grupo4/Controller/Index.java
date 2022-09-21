4aec47cee7b9cca5f03eb90b0b84710f0f9ef0ff
Jun23333333 <a20190212@pucp.edu.pe>
2022-09-20 20:55:39 -0500

    autowirreds

--- src/main/java/com/example/lab3_grupo4/Controller/Index.java
+++ src/main/java/com/example/lab3_grupo4/Controller/Index.java
@@ -1,6 +1,9 @@
 package com.example.lab3_grupo4.Controller;
 
 
+import com.example.lab3_grupo4.Entity.Job;
+import com.example.lab3_grupo4.Repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = {""})
 public class Index {
+
+    @Autowired
+    CountryRepository countryRepository;
+    @Autowired
+    DepartmentsRepository departmentsRepository;
+    @Autowired
+    EmployeesRepository employeesRepository;
+    @Autowired
+    JobsRepository jobsRepository;
+    @Autowired
+    Job_historyRepository job_historyRepository;
+    @Autowired
+    LocationsRepository locationsRepository;
+    @Autowired
+    RegionsRepository regionsRepository;
 
 
     @GetMapping(value = {""})
