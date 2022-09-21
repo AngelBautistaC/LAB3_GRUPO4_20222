b77a35f4b529d1c7e05b606bf5ebcd0f5f26cdd0
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-09-20 21:05:14 -0500

    lista empleados

--- src/main/java/com/example/lab3_grupo4/Controller/MainController.java
+++ src/main/java/com/example/lab3_grupo4/Controller/MainController.java
@@ -1,7 +1,46 @@
 package com.example.lab3_grupo4.Controller;
 
+import com.example.lab3_grupo4.Entity.Employee;
+import com.example.lab3_grupo4.Repository.EmployeesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class MainController {
+
+
+    @Autowired
+    EmployeesRepository employeesRepository;
+
+
+    @GetMapping(value = {"/listaEmpleado",""})
+    public String listaEmpleado(Model model){
+
+        model.addAttribute("listaEmpleados",employeesRepository.findAll());
+
+        return "employee/lista";
+
+    }
+
+    @PostMapping(value={"/search"})
+    public String buscarEmployee(@RequestParam("searchField") String searchField, Model model){
+        List<Employee> listaEmployees=employeesRepository.buscarEmployee(searchField);
+        model.addAttribute("listaEmpleados",listaEmployees);
+
+        return "employee/lista";
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/com/example/lab3_grupo4/Repository/EmployeesRepository.java
+++ src/main/java/com/example/lab3_grupo4/Repository/EmployeesRepository.java
@@ -2,8 +2,30 @@ package com.example.lab3_grupo4.Repository;
 
 import com.example.lab3_grupo4.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employee, Integer> {
+
+
+
+
+
+    @Transactional
+    @Query(value = """
+            select * from employees e
+            inner join jobs j
+            on (e.job_id=j.job_id)
+            inner join departments dep
+            on (e.department_id=dep.department_id)
+            inner join locations loc
+            on (dep.location_id=loc.location_id)
+            where LOWER(first_name) like LOWER(concat('%',?1,'%')) OR LOWER(last_name) like LOWER(concat('%',?1,'%')) OR LOWER(j.job_title) LIKE LOWER (concat('%',?1,'%')) OR LOWER(dep.department_name) LIKE LOWER(concat('%',?1,'%')) OR LOWER(loc.city) like LOWER(concat('%',?1,'%'))\s""",nativeQuery = true)
+    List<Employee> buscarEmployee(String searchField);
+
+
 }
