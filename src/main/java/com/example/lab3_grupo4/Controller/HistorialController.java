6931648a92c8525e490828e841241da4253173f9
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2022-09-20 21:11:38 -0500

    historial

---
+++ src/main/java/com/example/lab3_grupo4/Controller/HistorialController.java
@@ -0,0 +1,9 @@
+package com.example.lab3_grupo4.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HistorialController {
+
+
+}
---
+++ src/main/java/com/example/lab3_grupo4/DTos/HistorialRepo.java
@@ -0,0 +1,4 @@
+package com.example.lab3_grupo4.DTos;
+
+public interface HistorialRepo {
+}
