99971a5749a485246ccbb16fd5cd7fcd57f6ab93
MarceloJon <mrojasl@pucp.edu.pe>
2022-09-20 21:05:23 -0500

    crear empleado

--- src/main/java/com/example/lab3_grupo4/Controller/MainController.java
+++ src/main/java/com/example/lab3_grupo4/Controller/MainController.java
@@ -1,8 +1,14 @@
 package com.example.lab3_grupo4.Controller;
 
+import com.example.lab3_grupo4.Entity.Employee;
+import com.example.lab3_grupo4.Repository.DepartmentsRepository;
+import com.example.lab3_grupo4.Repository.EmployeesRepository;
+import com.example.lab3_grupo4.Repository.JobsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -10,11 +16,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    @Autowired
+    EmployeesRepository employeesRepository;
 
-    @GetMapping("")
-    public String newUser(){
+    @Autowired
+    DepartmentsRepository departmentsRepository;
+
+    @Autowired
+    JobsRepository jobsRepository;
+
+    @GetMapping("nuevo")
+    public String newUser(Model model){
+        model.addAttribute("listaJefes",employeesRepository.buscaJefes());
+        model.addAttribute("listaDepart",departmentsRepository.findAll());
+        model.addAttribute("listaJobs",jobsRepository.findAll());
 
         return "newuser";
+    }
+
+    @PostMapping("savenew")
+    public String saveUser(Employee employee){
+        employeesRepository.save(employee);
+        return "redirect:/empleado";
     }
 
 
--- src/main/java/com/example/lab3_grupo4/Repository/EmployeesRepository.java
+++ src/main/java/com/example/lab3_grupo4/Repository/EmployeesRepository.java
@@ -2,8 +2,14 @@ package com.example.lab3_grupo4.Repository;
 
 import com.example.lab3_grupo4.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EmployeesRepository extends JpaRepository<Employee, Integer> {
+
+    @Query(value="SELECT * FROM employees group by manager_id",nativeQuery = true)
+    List<Employee> buscaJefes();
 }
